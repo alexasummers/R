d9e8fc73341def9ae3d907e8b134ceaa1d8e2859
Alexa Summers <alexas0202@gmail.com>
2021-02-07 17:31:32 -0600

    Create Strings.R
    Create first R script with string examples

---
+++ Strings.R
@@ -0,0 +1,7 @@
+#defines a string variable myString, where we assign a string "Hello, world!"
+myString <- "Hello, world!"
+#Print() is being used to print the value stored in variable myString
+print(myString)
+#We can also save things using = rather than <- :
+myString2 = "My first script in R"
+print(myString2)
